74dfaa23088e8146784df3accbfbbb9dfa911d63
Ammar Shahab <ammar.qa@generalpharma.com>
2025-02-11 08:31:34 +0600

    all the task completed

--- task.js
+++ task.js
@@ -1,5 +1,5 @@
-/* *
-*Task 1
+/* 
+!Task 1
 Write a JavaScript code to reverse the array colors without using the reverse method.
 
 Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
@@ -18,19 +18,28 @@ for (const element of colors) {
 }
 console.log(reverse);
 
-/* *
-*Task 2
+/* 
+!Task 2
 Write a JavaScript code to get the even numbers from an array using any looping technique.
 
 Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
 
 Output:
 
-[12, 98, 76, 46]
+[12, 98, 78, 46]
 */
+const numbers = [12, 98, 5, 41, 23, 78, 46];
 
-/* *
-*Task 3
+let evenNum = [];
+for (const number of numbers) {
+  if (number % 2 === 0) {
+    evenNum.push(number);
+  }
+}
+console.log(evenNum);
+
+/* 
+!Task 3
 Use a for...of loop to concatenate all the elements of an array into a single string.
 
 Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
@@ -39,9 +48,16 @@ Output:
 
 'TomTimTinTik'
 */
+let names = ["Tom", "Tim", "Tin", "Tik"];
 
-/* *
-*Task 4 (Hard)
+let output = "";
+for (let name of names) {
+  output += name;
+}
+console.log(output);
+
+/* 
+!Task 4 (Hard)
 Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
 
 Input: const statement = 'I am a hard working person'
@@ -49,6 +65,22 @@ Input: const statement = 'I am a hard working person'
 Output:
 
 'person working hard a am I'
-
-
 */
+
+const statements = "I am a hard working person";
+
+const statementsToArr = statements.split(" ");
+console.log(statementsToArr);
+
+let reversed = [];
+let reverseSent = "";
+for (const statement of statementsToArr) {
+  reversed.unshift(statement);
+}
+// console.log(reversed);
+
+for (const element of reversed) {
+  reverseSent = reverseSent + element + " ";
+}
+
+console.log(reverseSent);
